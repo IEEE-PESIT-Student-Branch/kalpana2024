c2e6763de601665b7a0c44df949fa1a04fae7ce4
Jawahar Balachandher <jawahar.balachandher@gmail.com>
2024-03-17 09:50:00 +0530

    update folder name

---
+++ Kalpana2024-Magical-Contest-Official-Writeups/Python/flag.py
@@ -0,0 +1,4 @@
+a=chr(40)+chr(97)+chr(108)+chr(109)+chr(111)+chr(115)+chr(116)+chr(41)
+flag_enc = chr(57)+chr(52)+chr(59)+chr(88)+chr(85)+chr(64)+chr(59)+chr(93)+chr(76)+chr(47)+chr(48)+chr(74)+chr(89)+chr(74)+chr(51)+chr(64)+chr(53)+chr(86)+chr(79)+chr(76)+chr(48)+chr(106)+chr(101)+chr(79)+chr(55)+chr(71)+chr(67)+chr(57)
+
+printf('That is '+a+ ' correct: ' + flag_enc)
